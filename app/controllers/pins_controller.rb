ea86f35b06a364a89dd97b806007ad22b8af8891
jonathan fernandez <jhonypuga@gmail.com>
2015-08-28 13:19:04 -0400

    add pins/users association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   
   def index
@@ -11,7 +13,7 @@ class PinsController < ApplicationController
 
  
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
  
@@ -20,7 +22,7 @@ class PinsController < ApplicationController
 
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
 
 
     if @pin.save
@@ -40,17 +42,23 @@ class PinsController < ApplicationController
   end
 
  
-  def destroy
-    @pin.destroy
+ def destroy
+     @pin.destroy
      redirect_to pins_url , notice: 'Pin was successfully destroyed.' 
     end
-  end
+ end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
     end
+
+    def correct_user
+      @pin = currect_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "not authorized to edit this pin" if @pin.nil?
+    end
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
